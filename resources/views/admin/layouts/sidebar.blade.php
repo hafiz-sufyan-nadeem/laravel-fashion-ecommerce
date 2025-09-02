86f65eea059a3a174af758bf461b770fab8b0549
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-09-02 18:43:39 +0500

    add route of dashboard to sidebar logo

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -3,11 +3,11 @@
         <div class="sidebar-brand-icon">
             <img src={{asset('img/logo/logo2.png')}}>
         </div>
-        <div class="sidebar-brand-text mx-3">RuangAdmin</div>
+        <div class="sidebar-brand-text mx-3">RajaAdmin</div>
     </a>
     <hr class="sidebar-divider my-0">
     <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="{{route('admin.dashboard')}}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
