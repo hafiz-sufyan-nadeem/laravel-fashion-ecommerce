d858c203ff249ba8a979c4d5eb50feb9e4874d70
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-09-12 02:54:58 +0500

    add back button in see more page

--- resources/views/frontend/products/show.blade.php
+++ resources/views/frontend/products/show.blade.php
@@ -1,4 +1,18 @@
-<h1>{{$product->name}}</h1>
-<p>Price: {{$product->price}}</p>
-<p>Description: {{$product->description}}</p>
-<img src="{{asset('storage/'.$product->image)}}">
+<link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+>
+
+
+<div class="container mt-3" style="margin-left: 10px">
+    <h1>{{ $product->name }}</h1>
+    <p>Price: {{ $product->price }}</p>
+    <p>Description: {{ $product->description }}</p>
+    <img src="{{ asset('storage/'.$product->image) }}" alt="Product Image" class="img-fluid">
+
+    <br><br>
+
+    <a href="{{ route('home') }}" class="btn btn-primary">
+        Back
+    </a>
+</div>
