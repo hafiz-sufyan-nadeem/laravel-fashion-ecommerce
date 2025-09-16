728fc3e2435105bbfb6ba77364ef06eef55d43db
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-09-17 02:36:05 +0500

    add method to add item in cart in Cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+        $cartItem = CartItem::where('user_id', auth()->id())->where('product_id', $id)->first();
+
+        if ($cartItem) {
+            $cartItem->quantity += 1;
+            $cartItem->save();
+        }else{
+            CartItem::create([
+                'user_id' => auth()->id(),
+                'product_id' => $product->id,
+                'quantity' => 1,
+                'price' => $product->price,
+            ]);
+        }
+        return back()->with('success', 'Product added to cart successfully!');
+    }
 }
