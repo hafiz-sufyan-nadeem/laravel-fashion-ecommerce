27fe5efa4b77513cb07f8d2d18ad0fea149a6254
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-09-26 18:55:36 +0500

    add views/orders/show.blade to display detailes

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -125,7 +125,7 @@ class CartController extends Controller
         // Clear the cart
         CartItem::where('user_id', auth()->id())->delete();
 
-        return redirect()->route('home')->with('success', 'Order placed successfully!');
+        return redirect()->route('orders.show')->with('success', 'Order placed successfully!');
     }
 
 }
