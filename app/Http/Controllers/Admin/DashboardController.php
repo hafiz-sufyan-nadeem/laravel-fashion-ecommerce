131d8cf377acaf60cac28a386361d920dd18409a
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-10-09 00:46:24 +0500

    add method in DashboardController for count total_amount monthly & yearly

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class DashboardController extends Controller
 {
     public function index(){
+        $currentMonth = now()->month;
+        $currentYear = now()->year;
+
+//        ONLY DELIVERED ORDERS COUNT.
+        $earningMonthly = Order::whereMonth('created_at', $currentMonth)
+            ->whereYear('created_at', $currentYear)
+            ->where('status', 'delivered')
+            ->sum('total_amount');
+
         return view('admin.layouts.dashboard');
     }
 }
