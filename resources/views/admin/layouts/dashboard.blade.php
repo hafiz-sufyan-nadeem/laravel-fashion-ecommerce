d230cd9cb7b38de2263d51728a42f2f37a43adec
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-10-12 17:02:19 +0500

    add check to display totalCustomrs

--- resources/views/admin/layouts/dashboard.blade.php
+++ resources/views/admin/layouts/dashboard.blade.php
@@ -57,10 +57,10 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-uppercase mb-1">Total Customers</div>
-                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $total }}</div>
+                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $totalCustomers }}</div>
                             <div class="mt-2 mb-0 text-muted text-xs">
-                                <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
-                                <span>Since last month</span>
+                                <span class="text-primary mr-2"><i class="fa fa-users"></i></span>
+                                <span>Registered Users</span>
                             </div>
                         </div>
                         <div class="col-auto">
