07756c72223c0d65f739f2f79024150357cf3e62
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-10-17 15:36:56 +0500

    add skeleton for customers listing

---
+++ resources/views/admin/customers/index.blade.php
@@ -0,0 +1,16 @@
+@extends('admin.layouts.index')
+
+@section('content')
+    <div class="container-fluid">
+        <h1 class="h3 mb-4 text-gray-800">Customers</h1>
+
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Customer List</h6>
+            </div>
+            <div class="card-body">
+                <p>Customer data yahan show hoga (abhi test view hai).</p>
+            </div>
+        </div>
+    </div>
+@endsection
