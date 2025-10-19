69df2350969a228561151f4d20308f31bba5a2f1
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-10-19 18:47:35 +0500

    make Message relations in msg model and user for admin user msgs

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Message extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'admin_id',
+        'subject',
+        'message',
+        'is_read',
+    ];
+
+    public function sentMessages()
+    {
+        return $this->hasMany(Message::class, 'user_id');
+    }
+
+    public function receivedMessages()
+    {
+        return $this->hasMany(Message::class, 'admin_id');
+    }
+
 }
